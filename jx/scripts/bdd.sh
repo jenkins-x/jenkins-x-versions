863c0f4577224e8cd1fb63c45df20ad0977668e1
James Strachan <james.strachan@gmail.com>
2019-02-03 12:21:11 +0000

    add the bdd tests

---
+++ jx/scripts/bdd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+JX_HOME="/tmp/jxhome"
+KUBECONFIG="/tmp/jxhome/config"
+
+mkdir -p $JX_HOME
+
+jx --version
+jx step git credentials
+
+gcloud auth activate-service-account --key-file $SERVICE_ACCOUNT_FILE
+
+# lets setup git 
+git config --global --add user.name JenkinsXBot
+git config --global --add user.email jenkins-x@googlegroups.com
+
+echo "running the BDD tests with JX_HOME = $JX_HOME"
+
+jx step bdd --config jx/bdd/clusters.yaml --gopath /tmp --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
