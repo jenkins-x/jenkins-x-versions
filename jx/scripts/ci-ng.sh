6aecc069c4e388291ab915698ecc1cfe27631564
James Strachan <james.strachan@gmail.com>
2019-02-28 08:48:49 +0000

    added tekton and ng scripts

--- jx/scripts/ci-ng.sh
+++ jx/scripts/ci-ng.sh
@@ -26,4 +26,4 @@ git config --global --add user.email jenkins-x@googlegroups.com
 
 echo "running the BDD tests with JX_HOME = $JX_HOME"
 
-jx step bdd --config jx/bdd/prow.yaml --gopath /tmp --git-provider=github --git-username $GH_USERNAME --git-owner $GH_USERNAME --git-api-token $GH_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
+jx step bdd --config jx/bdd/ng.yaml --gopath /tmp --git-provider=github --git-username $GH_USERNAME --git-owner $GH_USERNAME --git-api-token $GH_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
