5e5acbcaa31726f087ca5d6b64c417674e224ece
JenkinsXBot <jenkins-x@googlegroups.com>
2019-03-06 19:00:03 +0000

    use better git owner for bdd tests on github

--- jx/scripts/ci-tekton.sh
+++ jx/scripts/ci-tekton.sh
@@ -3,6 +3,8 @@ set -e
 set -x
 
 export GH_USERNAME="jenkins-x-bot-test"
+export GH_OWNER="cb-kubecd"
+
 export GH_CREDS_PSW="$(jx step credential -s jenkins-x-bot-test-github)"
 export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
 export GKE_SA="$(jx step credential -s gke-sa)"
@@ -26,4 +28,4 @@ git config --global --add user.email jenkins-x@googlegroups.com
 
 echo "running the BDD tests with JX_HOME = $JX_HOME"
 
-jx step bdd --config jx/bdd/tekton.yaml --gopath /tmp --git-provider=github --git-username $GH_USERNAME --git-owner $GH_USERNAME --git-api-token $GH_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
+jx step bdd --config jx/bdd/tekton.yaml --gopath /tmp --git-provider=github --git-username $GH_USERNAME --git-owner $GH_OWNER --git-api-token $GH_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
