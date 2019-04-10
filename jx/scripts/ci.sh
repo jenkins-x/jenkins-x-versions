198c7f42f69b7285542afd60e21a638f557ffda7
James Strachan <james.strachan@gmail.com>
2019-04-10 18:20:36 +0100

    fix: use new SA and project

--- jx/scripts/ci.sh
+++ jx/scripts/ci.sh
@@ -4,7 +4,7 @@ set -x
 
 export GHE_CREDS_PSW="$(jx step credential -s jx-pipeline-git-github-ghe)"
 export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
-export GKE_SA="$(jx step credential -s gke-sa)"
+export GKE_SA="$(jx step credential -s bdd-secret)"
 
 echo "BRANCH_NAME=$BRANCH_NAME"
 
