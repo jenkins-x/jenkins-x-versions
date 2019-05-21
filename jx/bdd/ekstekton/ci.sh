618e932b5832ad77113f1911f171e8142eadcc7c
David Conde <dconde@cloudbees.com>
2019-05-21 14:08:25 +0100

    Add a test for eks classic

--- jx/bdd/ekstekton/ci.sh
+++ jx/bdd/ekstekton/ci.sh
@@ -7,8 +7,8 @@ export GH_OWNER="cb-kubecd"
 
 export GH_CREDS_PSW="$(jx step credential -s jenkins-x-bot-test-github)"
 export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
-export AWS_ACCESS_KEY_ID="$(jx step credential -s jx-bdd-aws -k AWS_ACCESS_KEY_ID)
-export AWS_SECRET_ACCESS_KEY="$(jx step credential -s jx-bdd-aws -k AWS_SECRET_ACCESS_KEY)
+export AWS_ACCESS_KEY_ID="$(jx step credential -s jx-bdd-aws -k AWS_ACCESS_KEY_ID)"
+export AWS_SECRET_ACCESS_KEY="$(jx step credential -s jx-bdd-aws -k AWS_SECRET_ACCESS_KEY)"
 
 
 # fix broken `BUILD_NUMBER` env var
@@ -22,12 +22,10 @@ mkdir -p $JX_HOME
 jx --version
 jx step git credentials
 
-gcloud auth activate-service-account --key-file $GKE_SA
-
 # lets setup git 
 git config --global --add user.name JenkinsXBot
 git config --global --add user.email jenkins-x@googlegroups.com
 
 echo "running the BDD tests with JX_HOME = $JX_HOME"
 
-jx step bdd --use-revision --versions-repo https://github.com/jenkins-x/jenkins-x-versions.git --config jx/bdd/ekstekton/cluster.yaml --gopath /tmp --git-provider=github --git-username $GH_USERNAME --git-owner $GH_OWNER --git-api-token $GH_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring
+jx step bdd --use-revision --versions-repo https://github.com/jenkins-x/jenkins-x-versions.git --config jx/bdd/ekstekton/cluster.yaml --gopath /tmp --git-provider=github --git-username $GH_USERNAME --git-owner $GH_OWNER --git-api-token $GH_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring --domain=bdd-$BUILD_NUMBER.jxbdd.beescloud.com
