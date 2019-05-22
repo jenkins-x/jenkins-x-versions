e9ff0b1b4628201aa2a4026a10a71df3142cf455
David Conde <dconde@cloudbees.com>
2019-05-22 15:51:09 +0100

    Attempt to set aws credentials for entire pod

--- jx/bdd/eksclassic/ci.sh
+++ jx/bdd/eksclassic/ci.sh
@@ -7,6 +7,9 @@ export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
 export AWS_ACCESS_KEY_ID="$(jx step credential -s jx-bdd-aws -k AWS_ACCESS_KEY_ID)"
 export AWS_SECRET_ACCESS_KEY="$(jx step credential -s jx-bdd-aws -k AWS_SECRET_ACCESS_KEY)"
 export AWS_REGION="us-east-1"
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_REGION
 
 
 # fix broken `BUILD_NUMBER` env var
