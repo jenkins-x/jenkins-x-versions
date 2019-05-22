c34df0a66edef5c86d74fbce730136414aa40627
David Conde <dconde@cloudbees.com>
2019-05-22 16:01:00 +0100

    Dont rely on aws binary

--- jx/bdd/eksclassic/ci.sh
+++ jx/bdd/eksclassic/ci.sh
@@ -7,10 +7,11 @@ export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
 export AWS_ACCESS_KEY_ID="$(jx step credential -s jx-bdd-aws -k AWS_ACCESS_KEY_ID)"
 export AWS_SECRET_ACCESS_KEY="$(jx step credential -s jx-bdd-aws -k AWS_SECRET_ACCESS_KEY)"
 export AWS_REGION="us-east-1"
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set default.region $AWS_REGION
-
+echo "[default]
+region = $AWS_REGION" >> ~/.aws/config
+echo "[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 
 # fix broken `BUILD_NUMBER` env var
 export BUILD_NUMBER="$BUILD_ID"
