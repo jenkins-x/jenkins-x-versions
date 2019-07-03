9de0b4a2673418512533564754fc9d3ce09002f5
Cai Cooper <caicooper82@gmail.com>
2019-07-03 16:02:10 +0000

    feat: allow release state of jx to be set in master pipeline
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

--- jx/scripts/release-jx.sh
+++ jx/scripts/release-jx.sh
@@ -6,11 +6,14 @@ export GH_OWNER="jenkins-x"
 export GH_REPO="jx"
 export GITHUB_ACCESS_TOKEN="$(cat /builder/home/git-token 2> /dev/null)"
 
-VERSION=$1
-RELEASE_ID=$(curl -f "https://api.github.com/repos/${GH_OWNER}/${GH_REPO}/releases/tags/${VERSION}?access_token=${GITHUB_ACCESS_TOKEN}" -s | jq .id)
+VERSION=$(cat packages/jx.yml | grep version | cut -d':' -f2 | tr -d ' ')
+RELEASE_ID=$(curl -f "https://api.github.com/repos/${GH_OWNER}/${GH_REPO}/releases/tags/${VERSION}?access_token=${GITHUB_ACCESS_TOKEN}" -s | jq '. | select(.prerelease == true) | .id')
 
-curl -X PATCH \
-     -H 'Content-Type: application/json' \
-     -d '{"prerelease": false}' \
-     -f \
-     "https://api.github.com/repos/cagiti/quickstart-go2/releases/${RELEASE_ID}?access_token=${GITHUB_ACCESS_TOKEN}"
+if [ ! -z $RELEASE_ID ]
+then
+  curl -X PATCH \
+       -H 'Content-Type: application/json' \
+       -d '{"prerelease": false}' \
+       -f \
+       "https://api.github.com/repos/cagiti/quickstart-go2/releases/${RELEASE_ID}?access_token=${GITHUB_ACCESS_TOKEN}"
+fi
