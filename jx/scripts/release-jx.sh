2bfb0324dbf8511381c46afb05625375d73a9343
Cai Cooper <caicooper82@gmail.com>
2019-08-28 10:32:33 +0000

    feat: update jx dependencies on full release of jx
    Signed-off-by: Cai Cooper <caicooper82@gmail.com>

---
+++ jx/scripts/release-jx.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+export GH_OWNER="jenkins-x"
+export GH_REPO="jx"
+export DEPENDENCY_MATRIX="dependency-matrix/matrix.md"
+
+if [ -f $DEPENDENCY_MATRIX ]
+then
+  JX_VERSION=$(sed "s:^.*$GH_OWNER\/$GH_REPO.*\[\([0-9.]*\)\].*$:\1:;t;d" $DEPENDENCY_MATRIX)
+  if [ ! -z $JX_VERSION ]
+  then
+    jx step update release-status github --owner $GH_OWNER --repository $GH_REPO --version $JX_VERSION --prerelease=false
+  fi
+fi
