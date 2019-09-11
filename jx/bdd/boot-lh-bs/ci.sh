86f50eb4baff4ae6132654d4ec06c6f42196b4c5
James Rawlings <rawlingsj80@gmail.com>
2019-09-11 10:45:19 +0100

    fix: mount secrets into a non workspace dir to avoid checking out code into the same dir and accidentily commiting keys

--- jx/bdd/boot-lh-bs/ci.sh
+++ jx/bdd/boot-lh-bs/ci.sh
@@ -8,7 +8,6 @@ export GH_EMAIL="jenkins-x@googlegroups.com"
 export GH_CREDS_PSW="$(jx step credential -s jx-pipeline-git-bitbucketserver-bs)"
 
 export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user)"
-export GKE_SA="$(jx step credential -k bdd-credentials.json -s bdd-secret)"
 
 # fix broken `BUILD_NUMBER` env var
 export BUILD_NUMBER="$BUILD_ID"
