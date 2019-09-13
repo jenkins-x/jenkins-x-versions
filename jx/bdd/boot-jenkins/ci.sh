2972d8fc0b60029fe6a3f17496c880cf239cdcb7
Hardy Ferentschik <hardy@ferentschik.de>
2019-09-13 16:11:41 +0200

    fix: using a GitHub user as reop owner in order to be able to create private repos
    See also https://github.com/jenkins-x/jx/issues/5476 and https://github.com/jenkins-x/jx/issues/5186

--- jx/bdd/boot-jenkins/ci.sh
+++ jx/bdd/boot-jenkins/ci.sh
@@ -4,7 +4,7 @@ set -x
 
 export GH_USERNAME="jenkins-x-bot-test"
 export GH_EMAIL="jenkins-x@googlegroups.com"
-export GH_OWNER="cb-kubecd"
+export GH_OWNER="jenkins-x-bot-test"
 
 # fix broken `BUILD_NUMBER` env var
 export BUILD_NUMBER="$BUILD_ID"
