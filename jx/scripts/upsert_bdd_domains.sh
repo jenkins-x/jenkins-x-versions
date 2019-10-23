02afdf425cd4e1f60fed77e66e7b4ee861bec5cd
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-10-23 14:08:56 +0200

    fix: update the script to create domains if they do not exist
    Signed-off-by: Cosmin Cojocar <cosmin.cojocar@gmx.ch>

--- jx/scripts/upsert_bdd_domains.sh
+++ jx/scripts/upsert_bdd_domains.sh
@@ -19,28 +19,10 @@ function get_domain_nameservers()
   gcloud dns managed-zones --project=$project list --filter=$domain --format=json | jq .[].nameServers
 }
 
-function get_recordset_nameservers()
-{
-  #TODO
-  echo "TODO"
-}
-
 function create_subdomain()
 {
   local domain=$1
   gcloud dns managed-zones --project=$BDD_PROJECT create "${domain//\./-}" --dns-name "${domain}." --description="managed-zone for bdd tests"
-}
-
-function create_recordset_ns_entry()
-{
-  #TODO
-  echo "TODO"
-}
-
-function create_managed_zone()
-{
-  #TODO
-  echo "TODO"
 }
 
 for prefix in "${PREFIXES[@]}"
@@ -50,12 +32,13 @@ do
 
   if [[ $exists -eq 0 ]]
   then
-    echo "$domain doesn't exist"
-    #create_subdomain "${domain}"
+    create_subdomain "${domain}"
+    echo "$domain created"
+    child_nameservers="$(get_domain_nameservers "${domain}" "${BDD_PROJECT}")"
+    echo "${child_nameservers}"
   else
     echo "$domain exists"
     child_nameservers="$(get_domain_nameservers "${domain}" "${BDD_PROJECT}")"
-    parent_nameservers="$(get_recordset_nameservers "${domain}" "${PARENT_PROJECT}")"
     echo "${child_nameservers}"
   fi
 done
