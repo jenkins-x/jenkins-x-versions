4beaf5cf31fc46451bc7caa482df9616e374a935
James Strachan <james.strachan@gmail.com>
2020-02-26 14:31:46 +0000

    fix: lets use a single container step
    for the `jx alpha boot` BDD test

--- jx/bdd/boot-alpha/ci.sh
+++ jx/bdd/boot-alpha/ci.sh
@@ -26,13 +26,44 @@ echo "running the BDD tests with JX_HOME = $JX_HOME"
 echo "https://$GH_USERNAME:$GH_ACCESS_TOKEN@github.com" > $JX_HOME/git/credentials
 
 
-# connect to the new cluster
+# lets create a new GKE cluster
+gcloud auth activate-service-account --key-file $GKE_SA
+
+export CREATED_TIME=$(date '+%a-%b-%d-%Y-%H-%M-%S')
 export PROJECT_ID=jenkins-x-bdd3
 export CLUSTER_NAME="${BRANCH_NAME,,}-$BUILD_NUMBER-bdd-alpha"
 export ZONE=europe-west1-c
+export LABELS="branch=${BRANCH_NAME,,},cluster=bdd-boot-alpha,create-time=${CREATED_TIME,,}"
 
-gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE --project $PROJECT_ID
-jx ns jx
+echo "creating cluster $CLUSTER_NAME with labels $LABELS"
+
+git clone https://github.com/jenkins-x-charts/jenkins-x-installer
+jenkins-x-installer/create_cluster.sh
+
+
+echo "using the version stream ref: $PULL_PULL_SHA"
+
+# create the boot git repository
+jxl boot create -b --provider=gke --version-stream-ref=$PULL_PULL_SHA --env-git-owner=$GH_OWNER --project=$PROJECT_ID --cluster=$CLUSTER_NAME --zone=$ZONE --out giturl.txt
+
+
+# import secrets...
+echo "secrets:
+  adminUser:
+    username: admin
+    password: $JENKINS_PASSWORD
+  hmacToken: $GH_ACCESS_TOKEN
+  pipelineUser:
+    username: $GH_USERNAME
+    token: $GH_ACCESS_TOKEN
+    email: $GH_EMAIL" > /tmp/secrets.yaml
+
+jxl boot secrets import -f /tmp/secrets.yaml
+
+# run the boot Job
+echo running: jxl boot run -b --git-url `cat giturl.txt`
+
+jxl boot run -b --git-url `cat giturl.txt` --job
 
 
 # TODO not sure we need this?
@@ -42,6 +73,8 @@ helm repo add jenkins-x https://storage.googleapis.com/chartmuseum.jenkins-x.io
 # Just run the node-http import test here
 export BDD_TEST_SINGLE_IMPORT="node-http"
 
+
+# lets run the BDD tests
 jx step bdd \
     --use-revision \
     --version-repo-pr \
