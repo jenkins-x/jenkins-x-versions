e23dd020c5baad6fa774e27e63d994cee9559499
James Strachan <james.strachan@gmail.com>
2020-02-29 15:09:31 +0000

    fix: add BDD test for alpha boot (#829)
    * fix: add a new BDD test for alpha boot
    
    * fix: use lower case branch name
    
    * fix: use lowercase branch name
    
    * fix: don't overwrite the BUILD_NUMBER
    
    * fix: use batch mode
    
    * fix: upgrade to latest image
    
    * fix: specify the project, cluster and region
    
    * fix: lets use a zone rather than region
    
    * fix: latest image
    
    * fix: latest image
    
    * fix: latest image
    
    * fix: disable the bdd tests creating a new team
    
    * fix: share git init logic for the bdd step too
    
    * fix: add labels to created clusters
    
    * fix: use correct labels
    
    * fix: add nexus password
    
    * fix: lets use a single container step
    
    for the `jx alpha boot` BDD test
    
    * fix: lets default to helm 3
    
    * fix: latest image
    
    * fix: set the namespace before running the bdd tests
    
    * fix: add diagnostic kubectl commands
    
    * fix: connect to the remote cluster
    
    * chore: try sleep to diagnose kubectl issues
    
    * fix: try fix get env
    
    * fix: add go install
    
    * fix: switch to using the new image + bddjx binary
    
    * fix: pipeline image
    
    * fix: pipeline image
    
    * fix: workaround for old env var
    
    * fix: lets run boot import/run from git clone
    
    * fix: lets use verbose logging on the bdd tests
    
    * fix: upgrade to latest jxl
    
    * fix: run with git url
    
    * fix: use giturl
    
    * fix: update image
    
    * fix: disable the app deletion for now

---
+++ jx/bdd/boot-alpha/README.md
@@ -0,0 +1,2 @@
+## BDD test using `jx alpha boot`
+
---
+++ jx/bdd/boot-alpha/ci.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# setup environment
+JX_HOME="/tmp/jxhome"
+KUBECONFIG="/tmp/jxhome/config"
+
+# lets avoid the git/credentials causing confusion during the test
+export XDG_CONFIG_HOME=$JX_HOME
+mkdir -p $JX_HOME/git
+
+jx --version
+
+export GH_USERNAME="jenkins-x-versions-bot-test"
+export GH_EMAIL="jenkins-x@googlegroups.com"
+export GH_OWNER="jenkins-x-versions-bot-test"
+
+# lets setup git
+git config --global --add user.name JenkinsXBot
+git config --global --add user.email jenkins-x@googlegroups.com
+
+echo "running the BDD tests with JX_HOME = $JX_HOME"
+
+# replace the credentials file with a single user entry
+echo "https://$GH_USERNAME:$GH_ACCESS_TOKEN@github.com" > $JX_HOME/git/credentials
+
+
+# lets create a new GKE cluster
+gcloud auth activate-service-account --key-file $GKE_SA
+
+export CREATED_TIME=$(date '+%a-%b-%d-%Y-%H-%M-%S')
+export PROJECT_ID=jenkins-x-bdd3
+export CLUSTER_NAME="${BRANCH_NAME,,}-$BUILD_NUMBER-bdd-alpha"
+export ZONE=europe-west1-c
+export LABELS="branch=${BRANCH_NAME,,},cluster=bdd-boot-alpha,create-time=${CREATED_TIME,,}"
+
+echo "creating cluster $CLUSTER_NAME with labels $LABELS"
+
+git clone https://github.com/jenkins-x-charts/jenkins-x-installer
+jenkins-x-installer/create_cluster.sh
+
+
+# TODO remove once we remove the code from the multicluster branch of jx:
+export JX_SECRETS_YAML=/tmp/secrets.yaml
+
+echo "using the version stream ref: $PULL_PULL_SHA"
+
+# create the boot git repository
+jxl boot create -b --provider=gke --version-stream-ref=$PULL_PULL_SHA --env-git-owner=$GH_OWNER --project=$PROJECT_ID --cluster=$CLUSTER_NAME --zone=$ZONE --env-git-public  --git-public --out giturl.txt
+
+# import secrets...
+echo "secrets:
+  adminUser:
+    username: admin
+    password: $JENKINS_PASSWORD
+  hmacToken: $GH_ACCESS_TOKEN
+  pipelineUser:
+    username: $GH_USERNAME
+    token: $GH_ACCESS_TOKEN
+    email: $GH_EMAIL" > /tmp/secrets.yaml
+
+jxl boot secrets import -f /tmp/secrets.yaml --git-url `cat giturl.txt`
+
+# run the boot Job
+echo running: jxl boot run -b --git-url `cat giturl.txt`
+
+jxl boot run -b --git-url `cat giturl.txt` --job
+
+
+# lets make sure jx defaults to helm3
+export JX_HELM3="true"
+
+gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE --project $PROJECT_ID
+jx ns jx
+
+# diagnostic commands to test the image's kubectl
+kubectl version
+
+# for some reason we need to use the full name once for the second command to work!
+kubectl get environments
+kubectl get env
+kubectl get env dev -oyaml
+
+# TODO not sure we need this?
+helm init
+helm repo add jenkins-x https://storage.googleapis.com/chartmuseum.jenkins-x.io
+
+
+export JX_DISABLE_DELETE_APP="true"
+
+# run the BDD tests
+bddjx -ginkgo.focus=golang -test.v
